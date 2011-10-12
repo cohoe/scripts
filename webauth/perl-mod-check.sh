8e90e905085c7305645a23d950ee7230345e4807
Grant Cohoe <cohoe@csh.rit.edu>
2011-10-11 21:07:22 -0400

    comment

--- webauth/perl-mod-check.sh
+++ webauth/perl-mod-check.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# Verifies that you have the proper Perl modules installed to
+# compile and run Stanford Webauth 4
+#
+# Grant Cohoe 2011 (www.grantcohoe.com)
+
 perl -mCGI::Application -e 1
 perl -mCGI::Application::Plugin::AutoRunmode -e 1
 perl -mCGI::Application::Plugin::Forward -e 1
