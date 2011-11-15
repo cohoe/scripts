3b169130a5b0946da85c6102d3bc631025ba23df
Grant Cohoe <cohoe@csh.rit.edu>
2011-11-15 13:12:26 -0500

    more kcrap files

--- misc/KCRAP/generate_config.pl
+++ misc/KCRAP/generate_config.pl
@@ -23,11 +23,13 @@ use warnings;
 
 my $realm = `cat /etc/krb5.conf | grep default_realm | awk '{print \$3}'`;
 chomp $realm;
-my $principal = `find /var/* -name principal`;
+my $principal = `find /var/* -name principal 2>/dev/null`;
 chomp $principal;
-my $stash = `find /var/* -name .k5.$realm`;
+my $stash = `find /var/* -name .k5.$realm 2>/dev/null`;
 chomp $stash;
 
-print "[kcrap_server]\n\tport = 1999\n\trealm = $realm\n\n";
+print "# KCRAP configuration file. \n# WARNING: This must be done on your KDC!\n";
+
+print "[kcrap_server]\n\tport = 89\n\trealm = $realm\n\n";
 
 print "[realms]\n\t$realm = {\n\t\tdatabase_name = $principal\n\t\tkey_stash_file = $stash\n\t}\n";
